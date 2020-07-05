c5c8fd13e22a9c5cdee1f3dabdbe84c79331bf33
Graham Markall <graham@big-grey.co.uk>
2020-07-05 10:05:42 +0100

    Remove libc headers

--- libpsn00b/include/assert.h
+++
@@ -1,6 +0,0 @@
-#ifndef _ASSERT_H
-#define _ASSERT_H
-
-void assert(int e);
-
-#endif
--- libpsn00b/include/ctype.h
+++
@@ -1,7 +0,0 @@
-#ifndef _CTYPE_H
-#define _CTYPE_H
-
-extern int tolower(int chr);
-extern int toupper(int chr);
-
-#endif
